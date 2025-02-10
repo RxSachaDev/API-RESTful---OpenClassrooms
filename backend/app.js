3789dbb89f499471f263cb5f297ead7e560b5e13
ROUX SACHA <sacha.work38@gmail.com>
2025-02-10 15:46:16 +0100

    Mise en du CRUD

--- backend/app.js
+++ backend/app.js
@@ -20,14 +20,16 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use('/api/stuff', (req, res, next) => {
+  app.get('/api/stuff', (req, res, next) => {
     Thing.find()
       .then(things => res.status(200).json(things))
       .catch(error => res.status(400).json({ error }));
   });
 
 app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
+  if (req.body._id) {
+    delete req.body._id;
+  }
   const thing = new Thing({
     ...req.body
   });
@@ -40,6 +42,18 @@ app.get('/api/stuff/:id', (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
     .then(thing => res.status(200).json(thing))
     .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
